23b7cd7aa02c57c57ea59beb24cbab266d57f09b
Di <didoesdigital+git@gmail.com>
2024-04-09 15:26:36 +1000

    Validate multiple single-stroke lessons

--- src/commands/validateLessons.ts
+++ src/commands/validateLessons.ts
@@ -6,39 +6,51 @@ import lessonTargetDataDir from "../consts/lessonTargetDataDir";
 import { StenoDictionary } from "src/shared/types";
 
 const longestSingleStrokeTarget = `${lessonTargetDataDir}/drills/longest-words-with-single-stroke-briefs/longest-words-with-single-stroke-briefs.json`;
+const singleStrokeTarget = `${lessonTargetDataDir}/drills/single-stroke-briefs/single-stroke-briefs.json`;
 
 /**
  * This command does common checks on lessons like single-stroke lessons should have only single-stroke outlines
  */
 const run = async () => {
-  const sourceContent = await fs.readFile(longestSingleStrokeTarget, "utf8");
+  const singleStrokeLessons = [singleStrokeTarget, longestSingleStrokeTarget];
 
-  let longestSingleStrokeLessonDict: null | StenoDictionary = null;
-  try {
-    longestSingleStrokeLessonDict = JSON.parse(sourceContent);
-  } catch (error) {
-    console.error(
-      `Error: there was an error parsing the source dictionaries index file. `,
-      error
-    );
-  }
+  singleStrokeLessons.forEach(async (singleStrokeLesson) => {
+    const sourceContent = await fs.readFile(singleStrokeLesson, "utf8");
 
-  if (longestSingleStrokeLessonDict === null) {
-    console.error("Longest single-stroke briefs lesson dictionary is null");
-    return;
-  }
-
-  for (const [outline, translation] of Object.entries(
-    longestSingleStrokeLessonDict
-  )) {
-    if (outline.includes("/")) {
-      throw new Error(
-        `❌ Longest Words with Single-Stroke Briefs lesson contains multi-stroke outline: ${outline}: ${translation}`
+    let dict: null | StenoDictionary = null;
+    try {
+      dict = JSON.parse(sourceContent);
+    } catch (error) {
+      console.error(
+        `Error: there was an error parsing the lesson file for validation. `,
+        error
       );
     }
-  }
 
-  console.log("✅ Lessons validated. Looks Good To Me!");
+    if (dict === null) {
+      console.error(
+        `Found null dictionary validating lesson: ${singleStrokeLesson}`
+      );
+
+      throw new Error(
+        `❌ Single-stroke lessons should have dictionary files to validate. `
+      );
+    }
+
+    for (const [outline, translation] of Object.entries(dict)) {
+      if (outline.includes("/")) {
+        const filename = singleStrokeLesson.replace(/.*\//, "");
+        console.error(
+          `❌ ${filename} contains multi-stroke outline: ${outline}: ${translation}`
+        );
+        throw new Error(
+          `❌ Single-stroke lessons should not have multi-stroke outlines. `
+        );
+      }
+    }
+  });
+
+  console.log("✅ Single-stroke lessons validated. Looks Good To Me!");
 };
 
 export default {
