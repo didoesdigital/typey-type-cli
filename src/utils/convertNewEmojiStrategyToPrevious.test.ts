c51ea23ccdd695b48d94f16a36a73063257817d5
Di <didoesdigital+git@gmail.com>
2024-10-24 18:57:21 +1000

    Special handle skin tone emoji, thumbs up/down

--- src/utils/convertNewEmojiStrategyToPrevious.test.ts
+++ src/utils/convertNewEmojiStrategyToPrevious.test.ts
@@ -29,7 +29,7 @@ describe("convertNewStrategyToPrevious", () => {
     expect(convertNewEmojiStrategyToPrevious(input)).toEqual(expected);
   });
 
-  it("takes new emoji strategy with simple emoji and returns previous strategy format", async () => {
+  it("takes new emoji strategy with flag emoji and returns previous strategy format", async () => {
     const input: NewEmojiStrategy = {
       "1f1f8-1f1f9": {
         "name": "flag: S\u00e3o Tom\u00e9 &amp; Pr\u00edncipe",
@@ -48,6 +48,88 @@ describe("convertNewStrategyToPrevious", () => {
         // The old emoji strategy file had this value for the keywords property:
         // "s\u00e3o tom\u00e9 and pr\u00edncipe country nation st flag other pr\u00edncipe sao tome",
         "keywords": "flag uc6",
+      },
+    };
+    expect(convertNewEmojiStrategyToPrevious(input)).toEqual(expected);
+  });
+
+  it("takes new emoji strategy with skin tone emoji 💇🏻 and returns previous strategy format", async () => {
+    const input: NewEmojiStrategy = {
+      "1f487-1f3fb": {
+        "name": "person getting haircut: light skin tone",
+        "category": "people",
+        "shortname": ":person_getting_haircut_tone1:",
+        "shortname_alternates": [":haircut_tone1:"],
+        "keywords": [
+          "barber",
+          "beauty",
+          "haircut",
+          "light skin tone",
+          "parlor",
+          "uc8",
+        ],
+        "unicode_output": "1f487-1f3fb",
+      },
+    };
+    const expected: PreviousEmojiStrategy = {
+      "haircut_tone1": {
+        "unicode": "1f487-1f3fb",
+        "shortname": ":haircut_tone1:",
+        "aliases": "",
+        // The old emoji strategy file had this value for the keywords property:
+        // "keywords": "haircut tone 1 female girl woman",
+        "keywords": "barber beauty haircut light skin tone parlor uc8",
+      },
+    };
+    expect(convertNewEmojiStrategyToPrevious(input)).toEqual(expected);
+  });
+
+  it("takes new emoji strategy with skin tone emoji and aliases 🏋🏻 and returns previous strategy format", async () => {
+    const input: NewEmojiStrategy = {
+      "1f3cb-1f3fb": {
+        "name": "person lifting weights: light skin tone",
+        "category": "activity",
+        "shortname": ":person_lifting_weights_tone1:",
+        "shortname_alternates": [":lifter_tone1:", ":weight_lifter_tone1:"],
+        "keywords": ["lifter", "light skin tone", "weight", "uc8"],
+        "unicode_output": "1f3cb-1f3fb",
+      },
+    };
+    const expected: PreviousEmojiStrategy = {
+      "lifter_tone1": {
+        "unicode": "1f3cb-1f3fb",
+        "shortname": ":lifter_tone1:",
+        "aliases": ":weight_lifter_tone1:",
+        // The old emoji strategy file had this value for the keywords property:
+        // "keywords": "weight lifter tone 1 bench press squats deadlift",
+        "keywords": "lifter light skin tone weight uc8",
+      },
+    };
+    expect(convertNewEmojiStrategyToPrevious(input)).toEqual(expected);
+  });
+
+  it("takes new emoji strategy with thumbs up and returns previous strategy format", async () => {
+    const input: NewEmojiStrategy = {
+      "1f44d": {
+        "name": "thumbs up",
+        "category": "people",
+        "shortname": ":thumbsup:",
+        "shortname_alternates": [":+1:", ":thumbup:", ":thumbs_up:"],
+        "keywords": ["+1", "hand", "thumb", "up", "uc6"],
+        "unicode_output": "1f44d",
+      },
+    };
+    const expected: PreviousEmojiStrategy = {
+      "thumbsup": {
+        "unicode": "1f44d",
+        "shortname": ":thumbsup:",
+        // The old emoji strategy file had these values for the aliases and
+        // keywords properties:
+        // "aliases": ":+1:",
+        // "keywords":
+        //   "thumbs up sign cool hand like yes +1 body person thumb thumbs up",
+        "aliases": ":+1: :thumbup: :thumbs_up:",
+        "keywords": "+1 hand thumb up uc6",
       },
     };
     expect(convertNewEmojiStrategyToPrevious(input)).toEqual(expected);
--- src/utils/convertNewEmojiStrategyToPrevious.ts
+++ src/utils/convertNewEmojiStrategyToPrevious.ts
@@ -6,12 +6,33 @@ const convertNewEmojiStrategyToPrevious = (
   const newEntries = Object.entries(emojisInNewStrategyFormat);
   const result: PreviousEmojiStrategy = {};
   for (const [unicode_output, newEntry] of newEntries) {
-    result[newEntry.shortname.replace(/:/g, "")] = {
-      unicode: unicode_output,
-      shortname: newEntry.shortname,
-      aliases: newEntry.shortname_alternates.join(" "),
-      keywords: newEntry.keywords.join(" "),
-    };
+    if (
+      newEntry.shortname.match(/_tone[0-9]:/) !== null &&
+      newEntry.shortname_alternates.length > 0
+    ) {
+      const shortname =
+        // Handle exceptions for thumbs up/down emoji `+1` and `-1` shortname alternates:
+        newEntry.shortname_alternates[0].startsWith(":+") ||
+        newEntry.shortname_alternates[0].startsWith(":-")
+          ? newEntry.shortname_alternates[1]
+          : newEntry.shortname_alternates[0];
+
+      const newKey = shortname.replace(/:/g, "");
+      result[newKey] = {
+        unicode: unicode_output,
+        shortname: shortname,
+        aliases: newEntry.shortname_alternates.slice(1).join(" "),
+        keywords: newEntry.keywords.join(" "),
+      };
+    } else {
+      const newKey = newEntry.shortname.replace(/:/g, "");
+      result[newKey] = {
+        unicode: unicode_output,
+        shortname: newEntry.shortname,
+        aliases: newEntry.shortname_alternates.join(" "),
+        keywords: newEntry.keywords.join(" "),
+      };
+    }
   }
 
   return result;
